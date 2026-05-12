5c2ffae38e00a89d4ff9135394b923088aa04a87
HideInTheMoon <150688911+HideInTheMoon@users.noreply.github.com>
2026-05-12 11:48:58 +0800

    fix: correct auth-config.js quotes

--- js/auth-config.js
+++ js/auth-config.js
@@ -4,8 +4,8 @@
  */
 
 const AUTH_CONFIG = {
-    passwordHash: '"'"'DISABLED_NO_PASSWORD_SET_IN_SECRETS'"'"',
+    passwordHash: 'DISABLED_NO_PASSWORD_SET_IN_SECRETS',
     sessionDuration: 7 * 24 * 60 * 60 * 1000,
-    storageKey: '"'"'arxiv_auth_token'"'"',
-    storageExpireKey: '"'"'arxiv_auth_expire'"'"'
+    storageKey: 'arxiv_auth_token',
+    storageExpireKey: 'arxiv_auth_expire'
 };
