550a33c2bc134cc8695b1cc231e1ff37e1ccfc1b
HideInTheMoon <150688911+HideInTheMoon@users.noreply.github.com>
2026-05-13 14:55:38 +0800

    fix: replace placeholder repo owner/name with actual values

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'HideInTheMoon',
+    repoOwner: 'dw-dengwei',
 
     /**
      * GitHub repository name
@@ -43,3 +43,4 @@ const DATA_CONFIG = {
         return `${this.getDataBaseUrl()}/${filePath}`;
     }
 };
+
